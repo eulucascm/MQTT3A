98b8f9d941aa67230da503cb0e0b6f0c5bdf03a4
LOGONPF <LOGONPF@colegiomodulo.com.br>
2024-03-18 08:59:32 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import paho.mqtt.client as mqtt
+
+def ao_conectar(client, userdata, flags, rc):
+    print("Nos conectamos com o seguinte código de resultado {}".format(str(rc)))
+
+
+def ao_receber(client, userdata, msg):
+     print("{} --- {}".format(msg.topic, str(msg.payload)))
