c7db839b918fab2606b2ce6a2d5b554bf9911861
luscaFusca <lucasgomes_2006@hotmail.com>
2024-03-25 09:03:51 -0300

    aula 25/03

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def ao_conectar(client, userdata, flags, rc):
 
 def ao_receber(client, userdata, msg):
      print("{} --- {}".format(msg.topic, str(msg.payload)))
+
+cliente = mqtt.Client()
+
+cliente.on_connect = ao_conectar
+cliente.on_message = ao_receber
+cliente.connect("broker.hivemq.com", 1883, 60)
+cliente.subscribe("aula3a")
+cliente.loop_forever()
+
